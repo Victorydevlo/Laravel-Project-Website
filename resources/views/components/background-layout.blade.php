1cbf8727857f0f228197300376e588f0a43b06b7
c3633460 <v.ukaegbu2496@student.leedsbeckett.ac.uk>
2024-11-08 16:03:53 +0000

    added a php file that will check if a user is admin or not

--- resources/views/components/background-layout.blade.php
+++ resources/views/components/background-layout.blade.php
@@ -1,3 +1,49 @@
-<div>
-    <!-- I begin to speak only when I am certain what I will say is not better left unsaid. - Cato the Younger -->
-</div>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <title>Laravel</title>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body>
+        <header>
+            @auth
+            <div class="sm:flex sm:items-center sm:ml-6">
+                <x-dropdown align="left" width="48">
+                    <x-slot name="trigger">
+                        <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                            <div>{{ Auth::user()->name }}</div>
+                            <div class="ml-1">
+                            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                            </svg>
+                            </div>
+                        </button>
+                    </x-slot>
+                    <x-slot name="content">
+                        <!-- Authentication -->
+                        <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <x-dropdown-link :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();">
+                        {{ __('Log Out') }}
+                        </x-dropdown-link>
+                        </form>
+                    </x-slot>
+                </x-dropdown>
+            </div>             
+@else
+    <div style="text-align: right;">
+     <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
+     @if (Route::has('register'))
+     <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+    </div>
+     @endif
+@endif
+        </header>
+        <section>
+            <p>MAIN CONTENT</p>
+        </section>
+        <footer>
+            <p>Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})</p>
+        </footer>
+    </body>
+</html>
