6719e0ab88c26c6a8cae618dc1654c47f8ed7e2c
c3633460 <v.ukaegbu2496@student.leedsbeckett.ac.uk>
2024-11-08 16:41:54 +0000

    when a user is loged in it will show the what their role is

--- resources/views/components/background-layout.blade.php
+++ resources/views/components/background-layout.blade.php
@@ -11,7 +11,9 @@
                 <x-dropdown align="left" width="48">
                     <x-slot name="trigger">
                         <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                            <div>{{ Auth::user()->name }}</div>
+                            <div>{{ Auth::user()->name }}
+                            </div>
+                            
                             <div class="ml-1">
                             <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                             <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
@@ -23,6 +25,11 @@
                         <!-- Authentication -->
                         <form method="POST" action="{{ route('logout') }}">
                         @csrf
+                                @if(Auth::user()->is_admin==1)
+                                Administrator
+                                @else
+                                Customer
+                                @endif
                         <x-dropdown-link :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();">
                         {{ __('Log Out') }}
                         </x-dropdown-link>
@@ -31,10 +38,13 @@
                 </x-dropdown>
             </div>             
 @else
-    <div style="text-align: right;">
-     <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
-     @if (Route::has('register'))
-     <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+    <div class="flex justify-between items-center ">
+        <h1 class="text-lg font-semibold ">Component 2</h1>
+        <div style="text-align: right;">
+            <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
+            @if (Route::has('register'))
+            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+        </div>
     </div>
      @endif
 @endif
@@ -46,4 +56,5 @@
             <p>Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})</p>
         </footer>
     </body>
+
 </html>
