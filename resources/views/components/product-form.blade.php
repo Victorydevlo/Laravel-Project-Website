eb8ca53ce3f0caa9b8ab6946627cd0d1624d2ed5
c3633460 <v.ukaegbu2496@student.leedsbeckett.ac.uk>
2024-11-18 15:41:55 +0000

    create a file that create will link to to view when clicked and used make:component to create another form blade file

--- resources/views/components/product-form.blade.php
+++ resources/views/components/product-form.blade.php
@@ -1,3 +1,18 @@
 <div>
-    <!-- Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less. - Marie Curie -->
+<form>  
+    <div class="p-2 m-2 rounded-lg border-2 border-blue-900 max-w-md">
+        <div class="text-sm">
+            <input name="title" type="text" placeholder="title" />
+        </div>
+        <p class="text-sm">
+            <input name="name" type="text" placeholder="artist/author/console" />
+        </p>
+        <p>
+            <input type="number" step='0.01' value='0.00' name="price"  placeholder="price" />
+        </p>   
+        <div>
+            <button type="submit" class="bg-gray-800 text-white mt-2 p-2">Add New</button>
+        </div>
+    </div>
+</form>
 </div>
