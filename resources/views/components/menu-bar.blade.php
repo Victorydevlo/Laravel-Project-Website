69318ea3e6e6aac887ecea3b4376f7287f406c34
c3633460 <v.ukaegbu2496@student.leedsbeckett.ac.uk>
2024-11-18 17:15:41 +0000

    Made it so only the admin can see the add product button and change its name to Add product

--- resources/views/components/menu-bar.blade.php
+++ resources/views/components/menu-bar.blade.php
@@ -3,9 +3,12 @@
     <div class="rounded-none bg-gray-700 hover:bg-blue-700 px-6 m-2 w-24">  
         <a href="{{ route('product') }}" class="text-center mx-auto text-gray-50">Home</a>
     </div>
+    @if(Auth::user()->is_admin==1)
     <div class="rounded-none bg-gray-700 hover:bg-blue-700 px-6 m-2 w-24">  
-        <a href="{{ route('create') }}" class="text-center mx-auto text-gray-50">About</a>
+        <a href="{{ route('create') }}" class="text-center mx-auto text-gray-50">Add Product</a>
     </div>
+    @else
+    @endif
     <div class="rounded-none bg-gray-700 hover:bg-blue-700 px-6 m-2 w-24">  
         <a href="{{ route('register') }}" class="text-center mx-auto text-gray-50">About</a>
     </div>    
