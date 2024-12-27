083ee14c5b3349f9bd4ec0902dcd3e16ddb519f6
c3633460 <v.ukaegbu2496@student.leedsbeckett.ac.uk>
2024-12-27 21:02:11 +0000

    added a filter button and additional bug fixes

--- resources/views/components/productspage.blade.php
+++ resources/views/components/productspage.blade.php
@@ -79,21 +79,50 @@
     justify-content: right;
 }          
 
+.filterbutton {
+    justify-content: left;
+}  
 </style>
 <section>            
 <div class="shadowlines"></div>
 <x-menu-bar />
 <div class="lines"></div>
+<div class="navbarstyle flex">
+    <div class="filterbutton">
+        <x-dropdown align="left" width="48">
+            <x-slot name="trigger">
+                <button class="justify-between" style="text-align: right; margin-right: 20px;">
+                    <div class="rounded-xl border border-gray-700 px-6 py-2 m-3 w-26 text-center">  
+                        <a class="text-gray-700 hover:text-blue-700 transition duration-300">Filter</a>
 
-@if(Route::is('productpage'))
-<div class="navbarstyle">
-<form action="{{ route('search') }}" method="GET">
-        
-    <div class="justify-between" style="text-align: right; margin-right: 20px;">
-    <input type="text" name="search" placeholder="Search Products">
-    <button type="submit" class='rounded-none border border-gray-700 px-6 py-2 m-3 w-26 text-center'>Search</button>
+                        <div class="ml-3 flex">
+                            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                            </svg>
+                        </div>                        
+                    </div>
+                </button>
+            </x-slot>
+            <x-slot name="content" style='overflow:hidden; width:100%; height:500px; position:relative;'>
+                <form method="POST" action="{{ route('logout') }}">
+
+                    <div style="text-align: right;">
+                    </div>
+                </form>
+            </x-slot>
+        </x-dropdown>
     </div>
-</form>
+
+    @if(Route::is('productpage'))
+    <div class="navbarstyle">
+        <form action="{{ route('search') }}" method="GET">
+        
+            <div class="justify-between" style="text-align: right; margin-right: 20px;">
+                <input type="text" name="search" placeholder="Search Products">
+                    <button type="submit" class='rounded-none border border-gray-700 px-6 py-2 m-3 w-26 text-center'>Search</button>
+            </div>
+        </form>
+    </div>
 </div>
 @endif
 
